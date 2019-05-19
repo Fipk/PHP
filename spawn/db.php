98c111057f577de335cb2af43a256ed8f8a4dfed
Calvin <calvin.seaphanh@ynov.com>
2019-05-19 02:36:34 +0200

    Spawn

---
+++ spawn/db.php
@@ -0,0 +1,6 @@
+<?php
+define("DATABASE_HOST", "localhost");
+define("DATABASE_NAME", "spawn");
+define("DATABASE_USER", "root");
+define("DATABASE_PASSWORD", "OUI");
+$db = new PDO('mysql:host='.DATABASE_HOST.';dbname='.DATABASE_NAME.';charset=utf8', DATABASE_USER, DATABASE_PASSWORD);
